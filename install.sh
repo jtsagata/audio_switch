e4a18584e7166b961b86725b50ec95ffa3dd9464
Ioannis Tsagkatakis <tsagatakis@protonmail.com>
2022-03-17 23:59:43 +0200

    Also install wireplumber, universal sound

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 wget https://raw.githubusercontent.com/jtsagata/audio_switch/master/audio_switch -O ${HOME}/.local/bin/audio_switch
 chmod +x ${HOME}/.local/bin/audio_switch
 
-echo "Type: ~/.local/bin/audio_switch repo to install the PPAs"
+echo "Type: ~/.local/bin/audio_switch repos to install the PPAs"
 echo "Type: ~/.local/bin/audio_switch install  to install the pipewire daemon"
 echo "Type: ~/.local/bin/audio_switch help to see other options"
